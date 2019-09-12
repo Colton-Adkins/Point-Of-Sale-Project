eccbd1e7baaaff1c6752760a7be84ed1e30dec40
Colton <coadkins20@sciototech.org>
2019-09-12 11:56:02 -0400

    0.1

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function HideDrinks() {
+  var drinks = document.getElementById("drinks");
+  if (drinks.style.display === "none") {
+    drinks.style.display = "inline-block";
+  } else {
+    drinks.style.display = "none";
+  }
+} 
+
