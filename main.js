5268484f5d87a89d66ef4997640eea05c44417a5
Colton <coadkins20@sciototech.org>
2019-09-12 12:41:11 -0400

    0.2

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+function SetStyle() {
+drinks.style.display = "none";   
+ic.style.display = "none";   
+chips.style.display = "none";   
+candy.style.display = "none";   
+}
+
 
 function HideDrinks() {
   var drinks = document.getElementById("drinks");
+
   if (drinks.style.display === "none") {
     drinks.style.display = "inline-block";
   } else {
@@ -8,3 +16,31 @@ function HideDrinks() {
   }
 } 
 
+function HideChips() {
+  var drinks = document.getElementById("chips");
+
+  if (chips.style.display === "none") {
+    chips.style.display = "inline-block";
+  } else {
+    chips.style.display = "none";
+  }
+} 
+
+function HideIC() {
+  var drinks = document.getElementById("ic");
+
+  if (ic.style.display === "none") {
+    ic.style.display = "inline-block";
+  } else {
+    ic.style.display = "none";
+  }
+} 
+function HideCandy() {
+  var drinks = document.getElementById("candy");
+
+  if (candy.style.display === "none") {
+    candy.style.display = "inline-block";
+  } else {
+    candy.style.display = "none";
+  }
+} 
